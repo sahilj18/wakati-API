225e70ac2c106ed8464a7d64f64aabfba1b3c2c4
prasadharbare <prasad1523@proton.me>
2025-01-08 09:03:59 +0000

    Added favicon

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import {notFound,onError} from "stoker/middlewares"
 import logger from "./lib/logger"
 // import { logger } from 'hono/logger'
 import { pinoLogger } from 'hono-pino'
+import {serveEmojiFavicon} from 'stoker/middlewares'
 
 const app = new OpenAPIHono()
-app.use (logger())
+app.use(logger())
+app.use(serveEmojiFavicon("👻"))
 app.notFound(notFound)
 app.onError(onError)
 
 app.get('/', (c) => {
-  return c.text('Hello Advait. Thanks for using Hono!')
+  return c.text('Thanks for using Hono!')
 })
 
 
