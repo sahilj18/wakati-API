d97245f02858ca8256638c9ebfc6d2e96965be21
DeepakShinde123 <Deepakshinde123@proton.me>
2025-01-08 08:50:02 +0000

    show unique request id for pino logs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
 import {notFound,onError} from "stoker/middlewares"
+// import { logger } from 'hono/logger'
+import { pinoLogger } from 'hono-pino'
 
 const app = new OpenAPIHono()
-
+app.use(pinoLogger(
+  {http:{
+    reqId: () => crypto.randomUUID()
+  }}
+))
 app.notFound(notFound)
 app.onError(onError)
 
 app.get('/', (c) => {
+  c.var.logger.warn("Hello there")
   return c.text('Hello Advait. Thanks for using Hono!')
 })
 
