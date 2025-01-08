929e311b279db4ee28ab5c6dd894341f49c5c84c
Advait Parab <advait0012@proton.me>
2025-01-08 08:14:37 +0000

    stoker library used

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import { Hono } from 'hono'
+import { OpenAPIHono } from '@hono/zod-openapi'
+import {notFound,onError} from "stoker/middlewares"
 
-const app = new Hono()
+const app = new OpenAPIHono()
+
+app.notFound(notFound)
+app.onError(onError)
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Advait. Thanks for using Hono!')
+})
+
+
+
+app.get("/error",(c)=>{
+  throw new Error("error")
 })
 
 export default app
