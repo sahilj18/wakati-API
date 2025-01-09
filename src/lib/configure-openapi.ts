f4f56dcdd34e2f5b4525dc9ca94022780f6b16ba
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2025-01-09 08:16:23 +0000

    customize scalar theme

--- src/lib/configure-openapi.ts
+++ src/lib/configure-openapi.ts
@@ -13,6 +13,7 @@ export default function configureOpenAPI(app: OpenAPIHono) {
     })
     app.get("reference", 
         apiReference({
+            theme: "elysiajs",
             spec:{
                 url:"/doc"
             }
