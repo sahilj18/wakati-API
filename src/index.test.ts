96d2bcdec1a2bd4a626d45996941c9cdf05175c7
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2025-01-16 07:29:03 +0000

    add testcase for analyze

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,28 @@
 import { describe, expect, test } from "vitest"
 import app from "./index"
 
-describe("Example",()=>{
-    test("GET /posts",async()=>{
+describe("Example", () => {
+    test("GET /posts", async () => {
         const res = await app.request("/status")
         expect(res.status).toBe(200)
         expect(await res.json()).toEqual({ message: "Wakati-API is active" })
     })
 })
+
+test("POST /analyze", async () => {
+    const res = await app.request("/analyze", {
+        method: "POST",
+        body: JSON.stringify({
+            text: "Hello there working on Hono."
+        }),
+        headers: new Headers({
+            "Content-Type": "application/json"
+        })
+    });
+
+    expect(res.status).toBe(200);
+
+    const json = await res.json();
+    expect(json).toHaveProperty("ari");
+    expect(json).toMatchObject({ wordCount: 5 });
+});
